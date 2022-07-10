efec2d259a2d47963d6bd95a135863af937d5a8c
selim13 <selim013@gmail.com>
2022-07-10 11:47:38 +0300

    chore: test composite actions

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,6 @@
 BACKUPS_DIR="$1"
 
 DBFILE=$(sudo find $BACKUPS_DIR -type f -size +1b -name '*testdb*.gz')
-echo FILE: $DBFILE
+
 [ "$DBFILE" != "" ] || { echo "::error::Backups not created" && exit 1; }
-echo GZ
 zgrep ignoretable "$DBFILE" && { echo "::error::Ignored table found in the backup" && exit 1; } || true
